ead8a33f44bb386c041b38ce03c216cde179c6ce
Caleb Fung <caleb.t.fung@gmail.com>
2017-11-30 18:40:32 -0600

    quick UI fixes

--- src/components/titlebar.js
+++ src/components/titlebar.js
@@ -5,7 +5,9 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import SvgIcon from 'material-ui/SvgIcon';
+import Tooltip from 'material-ui/Tooltip';
 
 
 const styles = {
@@ -23,21 +25,32 @@ const styles = {
   }
 }
 
+const MailIcon = (props) => (
+  <SvgIcon {...props}>
+    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"/>
+  </SvgIcon>
+)
+
+
+const TutorIcon = (props) => (
+  <SvgIcon {...props}>
+    <path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"/>
+  </SvgIcon>
+)
+
 function ButtonAppBar(props) {
   const classes = props.classes;
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar disableGutters>
-          {/* <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
-            <MenuIcon />
-          </IconButton> */}
           <img src="/logo.png" alt="logo" className={classes.menuButton}/>
           <Typography type="title" color="inherit">
             <Link to="/" style={{textDecoration:'none', border:'none', color:'white', marginRight:'30px'}}>IEEE UTD</Link>
           </Typography>
           <div className={classes.flex}></div>
-          <Button color="contrast"><Link to="/mailing-list" style={{textDecoration:'none', border:'none', color:'white'}}>Sign up!</Link></Button>
+          <Tooltip id="tooltip-tutoring" title="View our tutoring schedules!" placement="bottom"><IconButton><Link to="/tutoring" style={{textDecoration:'none', border:'none', color:'white'}}><TutorIcon /></Link></IconButton></Tooltip>
+          <Tooltip id="tooltip-tutoring" title="Join our mailing list!" placement="bottom"><IconButton><Link to="/mailing-list" style={{textDecoration:'none', border:'none', color:'white'}}><MailIcon /></Link></IconButton></Tooltip>
         </Toolbar>
       </AppBar>
     </div>
--- src/containers/Tutoring.js
+++ src/containers/Tutoring.js
@@ -6,7 +6,7 @@ import './Tutoring.css'
 class Tutoring extends React.Component {
   render() {
     return (
-      <div className="Tutoring">
+      <div className="Tutoring" style={{paddingBottom:'3em'}}>
         <h1>Tutoring</h1>
         <p>We offer tutoring at <a href="https://www.google.com/maps/place/Synergy+Park+North+Building+-+UT+Dallas/@32.9889742,-96.7559906,15z/data=!4m5!3m4!1s0x864c2206e04feeb9:0x54e35d04c429ba8a!8m2!3d32.9939073!4d-96.752337" target="_blank" rel="noopener noreferrer">JSSEC</a>.</p>
         <br/>
@@ -16,7 +16,7 @@ class Tutoring extends React.Component {
         <br/>
         <h2>Tutoring Schedules</h2>
         <p>We offer tutoring for the following classes:</p>
-        <ul>
+        <ul style={{marginBottom:0}}>
           <li><a href="https://docs.google.com/gview?embedded=true&url=https://www.ieeeutd.org/tutoring/ee-ce_2310.pdf" target="_blank" rel="noopener noreferrer">EE/CE 2310</a> <a href="/tutoring/ee-ce_2310.pdf" download>(Download)</a></li>
           <li><a href="https://docs.google.com/gview?embedded=true&url=https://www.ieeeutd.org/tutoring/ee-ce_3301.pdf" target="_blank" rel="noopener noreferrer">EE/CE 3301</a> <a href="/tutoring/ee-ce_3301.pdf" download>(Download)</a></li>
           <li><a href="https://docs.google.com/gview?embedded=true&url=https://www.ieeeutd.org/tutoring/ee-ce_3302.pdf" target="_blank" rel="noopener noreferrer">EE/CE 3302</a> <a href="/tutoring/ee-ce_3302.pdf" download>(Download)</a></li>
